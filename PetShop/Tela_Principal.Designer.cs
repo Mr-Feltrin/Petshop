b348757ed52deb9480f35f97c21bd99f4921d05a
Ufologista <45573198+Ufologista@users.noreply.github.com>
2019-01-30 20:11:04 -0200

    Adição rodapé menu inicial

--- PetShop/Tela_Principal.Designer.cs
+++ PetShop/Tela_Principal.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tela_Principal));
             this.menu_principal = new System.Windows.Forms.ToolStrip();
             this.botao_clientes = new System.Windows.Forms.ToolStripButton();
@@ -36,7 +37,18 @@
             this.botao_agenda = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.botao_produtos = new System.Windows.Forms.ToolStripButton();
+            this.status_rodape_principal = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatus_data_completa = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus_num_lock = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus_caps_lock = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus_data = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus_horario = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus_nome_administrador = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus_nome_sistema = new System.Windows.Forms.ToolStripStatusLabel();
+            this.horario_atual = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatus_agendamentos_atuais = new System.Windows.Forms.ToolStripStatusLabel();
             this.menu_principal.SuspendLayout();
+            this.status_rodape_principal.SuspendLayout();
             this.SuspendLayout();
             // 
             // menu_principal
@@ -123,6 +135,139 @@
             this.botao_produtos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.botao_produtos.ToolTipText = "Produtos";
             // 
+            // status_rodape_principal
+            // 
+            this.status_rodape_principal.AllowMerge = false;
+            this.status_rodape_principal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatus_data_completa,
+            this.toolStripStatus_agendamentos_atuais,
+            this.toolStripStatus_num_lock,
+            this.toolStripStatus_caps_lock,
+            this.toolStripStatus_data,
+            this.toolStripStatus_horario,
+            this.toolStripStatus_nome_administrador,
+            this.toolStripStatus_nome_sistema});
+            this.status_rodape_principal.Location = new System.Drawing.Point(0, 651);
+            this.status_rodape_principal.Name = "status_rodape_principal";
+            this.status_rodape_principal.Size = new System.Drawing.Size(1184, 27);
+            this.status_rodape_principal.SizingGrip = false;
+            this.status_rodape_principal.TabIndex = 2;
+            this.status_rodape_principal.Text = "statusStrip1";
+            // 
+            // toolStripStatus_data_completa
+            // 
+            this.toolStripStatus_data_completa.AutoSize = false;
+            this.toolStripStatus_data_completa.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_data_completa.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_data_completa.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_data_completa.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatus_data_completa.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_data_completa.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_data_completa.Name = "toolStripStatus_data_completa";
+            this.toolStripStatus_data_completa.Size = new System.Drawing.Size(727, 22);
+            this.toolStripStatus_data_completa.Spring = true;
+            this.toolStripStatus_data_completa.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatus_num_lock
+            // 
+            this.toolStripStatus_num_lock.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_num_lock.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_num_lock.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_num_lock.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_num_lock.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripStatus_num_lock.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_num_lock.Name = "toolStripStatus_num_lock";
+            this.toolStripStatus_num_lock.Size = new System.Drawing.Size(44, 22);
+            this.toolStripStatus_num_lock.Text = "Num";
+            // 
+            // toolStripStatus_caps_lock
+            // 
+            this.toolStripStatus_caps_lock.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_caps_lock.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_caps_lock.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_caps_lock.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_caps_lock.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_caps_lock.Name = "toolStripStatus_caps_lock";
+            this.toolStripStatus_caps_lock.Size = new System.Drawing.Size(47, 22);
+            this.toolStripStatus_caps_lock.Text = "Caps";
+            this.toolStripStatus_caps_lock.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatus_data
+            // 
+            this.toolStripStatus_data.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_data.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_data.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_data.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_data.Name = "toolStripStatus_data";
+            this.toolStripStatus_data.Size = new System.Drawing.Size(4, 22);
+            // 
+            // toolStripStatus_horario
+            // 
+            this.toolStripStatus_horario.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_horario.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_horario.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_horario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_horario.Name = "toolStripStatus_horario";
+            this.toolStripStatus_horario.Size = new System.Drawing.Size(4, 22);
+            // 
+            // toolStripStatus_nome_administrador
+            // 
+            this.toolStripStatus_nome_administrador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.toolStripStatus_nome_administrador.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_nome_administrador.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_nome_administrador.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_nome_administrador.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripStatus_nome_administrador.Image = global::PetShop.Properties.Resources.administrador_sistema;
+            this.toolStripStatus_nome_administrador.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripStatus_nome_administrador.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_nome_administrador.Name = "toolStripStatus_nome_administrador";
+            this.toolStripStatus_nome_administrador.Size = new System.Drawing.Size(20, 22);
+            // 
+            // toolStripStatus_nome_sistema
+            // 
+            this.toolStripStatus_nome_sistema.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_nome_sistema.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_nome_sistema.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_nome_sistema.Image = global::PetShop.Properties.Resources.computador_sistema;
+            this.toolStripStatus_nome_sistema.Name = "toolStripStatus_nome_sistema";
+            this.toolStripStatus_nome_sistema.Size = new System.Drawing.Size(113, 22);
+            this.toolStripStatus_nome_sistema.Text = "Pet Shop 1.0";
+            this.toolStripStatus_nome_sistema.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // horario_atual
+            // 
+            this.horario_atual.Enabled = true;
+            this.horario_atual.Interval = 1000;
+            this.horario_atual.Tick += new System.EventHandler(this.horario_atual_Tick);
+            // 
+            // toolStripStatus_agendamentos_atuais
+            // 
+            this.toolStripStatus_agendamentos_atuais.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatus_agendamentos_atuais.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatus_agendamentos_atuais.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatus_agendamentos_atuais.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatus_agendamentos_atuais.Image = global::PetShop.Properties.Resources.agenda_icon;
+            this.toolStripStatus_agendamentos_atuais.Name = "toolStripStatus_agendamentos_atuais";
+            this.toolStripStatus_agendamentos_atuais.Size = new System.Drawing.Size(208, 22);
+            this.toolStripStatus_agendamentos_atuais.Text = "Agendamentos para hoje: 0";
+            this.toolStripStatus_agendamentos_atuais.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // Tela_Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -131,15 +276,19 @@
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1184, 614);
+            this.ClientSize = new System.Drawing.Size(1184, 678);
+            this.Controls.Add(this.status_rodape_principal);
             this.Controls.Add(this.menu_principal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Tela_Principal";
             this.Text = "Pet Shop";
             this.Load += new System.EventHandler(this.Tela_Principal_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Tela_Principal_KeyDown);
             this.menu_principal.ResumeLayout(false);
             this.menu_principal.PerformLayout();
+            this.status_rodape_principal.ResumeLayout(false);
+            this.status_rodape_principal.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,6 +303,16 @@
         private System.Windows.Forms.ToolStripButton botao_agenda;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton botao_produtos;
+        private System.Windows.Forms.StatusStrip status_rodape_principal;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_data_completa;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_caps_lock;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_num_lock;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_data;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_horario;
+        private System.Windows.Forms.Timer horario_atual;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_nome_administrador;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_nome_sistema;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus_agendamentos_atuais;
     }
 }
 
--- PetShop/Tela_Principal.cs
+++ PetShop/Tela_Principal.cs
@@ -17,38 +17,35 @@ namespace PetShop
             InitializeComponent();
         }
 
-        private void toolStripButton1_Click(object sender, EventArgs e)
-        {
 
-
-        }
-
-        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
 
         private void Tela_Principal_Load(object sender, EventArgs e)
         {
+            // Verificação de data em formato completo
+            toolStripStatus_data_completa.Text = DateTime.Now.ToString("dddd, dd 'de' MMMM 'de' yyyy");
+            // Verificação de tecla Caps Lock ativa/inativa
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                toolStripStatus_caps_lock.ForeColor = SystemColors.ActiveCaptionText;
+            }
+            else
+            {
+                toolStripStatus_caps_lock.ForeColor = SystemColors.InactiveCaption;
+            }
+            // Verificação de tecla Num Lock ativa/inativa
+            if (Control.IsKeyLocked(Keys.NumLock))
+            {
+                toolStripStatus_num_lock.ForeColor = SystemColors.ActiveCaptionText;
+            }
+            else
+            {
+                toolStripStatus_num_lock.ForeColor = SystemColors.InactiveCaption;
+            }
+            // Verificação de data em formato numerico
+            toolStripStatus_data.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            // Exibição nome de usuário do sistema
+            toolStripStatus_nome_administrador.Text = Environment.UserName;
 
-            
-
-
-
-        }
-
-        private void clienteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void toolStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
-        private void toolStripButton1_Click_1(object sender, EventArgs e)
-        {
 
         }
 
@@ -56,6 +53,40 @@ namespace PetShop
         {
             Adicionar_cliente add = new Adicionar_cliente();
             add.ShowDialog();
+        }
+
+        private void Tela_Principal_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Verifica se tecla Caps Lock está ativa
+            if ((e.KeyCode & Keys.KeyCode) == Keys.CapsLock)
+            {
+                if (Control.IsKeyLocked(Keys.CapsLock))
+                {
+                    toolStripStatus_caps_lock.ForeColor = SystemColors.ActiveCaptionText;
+                }
+                else
+                {
+                    toolStripStatus_caps_lock.ForeColor = SystemColors.InactiveCaption;
+                }
+            }
+            // Verifica se tecla Num Lock está ativa
+            if ((e.KeyCode & Keys.KeyCode) == Keys.NumLock)
+            {
+                if (Console.NumberLock)
+                {
+                    toolStripStatus_num_lock.ForeColor = SystemColors.ActiveCaptionText;
+                }
+                else
+                {
+                    toolStripStatus_num_lock.ForeColor = SystemColors.InactiveCaption;
+                }
+            }
+
+        }
+        // Display de horário atual
+        private void horario_atual_Tick(object sender, EventArgs e)
+        {
+            this.toolStripStatus_horario.Text = DateTime.Now.ToString("HH:mm:ss");
         }
     }
 }
